386d076a2c16138d20f65a4dabf727b8fc74865c
Zakaria moussammi <90983110+zmoussam@users.noreply.github.com>
2022-03-25 18:26:38 +0100

    minitalk42

---
+++ minitalk/partie_bonus/utils_bonus.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_bonus.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zmoussam <zmoussam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/11 21:41:57 by zmoussam          #+#    #+#             */
+/*   Updated: 2022/03/25 18:10:42 by zmoussam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk_bonus.h"
+
+ static int	check_nbr(unsigned long nbr, int signe)
+{
+	if (signe < 0)
+	{
+		if (nbr == 2147483648)
+			return (-2147483648);
+		return (0);
+	}
+	return (-1);
+}
+
+int	ft_atoi(char *str)
+{
+	unsigned long	nombre;
+	int				signe;
+	int				i;
+
+	i = 0;
+	nombre = 0;
+	signe = 1;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			signe = signe * -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nombre = nombre * 10;
+		nombre = nombre + (int)(str[i] - '0');
+		i++;
+	}
+	if (nombre >= 2147483648)
+		return (check_nbr(nombre, signe));
+	return (nombre * signe);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write (1, "-2147483648", 11);
+	}
+	else if (nb < 0)
+	{
+		nb = -nb;
+		ft_putchar('-');
+		ft_putnbr(nb);
+	}
+	else if (nb <= 9)
+	{
+		ft_putchar(nb + '0');
+	}
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+}
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+void	ft_putstr(char *s)
+{
+	int	i;
+
+	if (!s)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		ft_putchar(s[i]);
+		i++;
+	}
+}
